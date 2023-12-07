a12d3abecff3b4f565c8ecdc346835bb45c62504
arve <arve.vesteraas@hotmail.com>
2023-12-07 16:14:05 +0100

    minor text

--- web.py
+++ web.py
@@ -13,7 +13,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase yot productivity")
+st.write("This app is to increase your productivity")
 
 
 for index, todo in enumerate(todos):
